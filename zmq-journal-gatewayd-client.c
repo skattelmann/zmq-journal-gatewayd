db6ccd5f475d9b5cb6d45b185b09e6c5ea52d932
Sascha Kattelmann <skattelmann@gmail.com>
2014-02-23 23:17:36 +0100

    finished client with heartbeating

--- zmq-journal-gatewayd-client.c
+++ zmq-journal-gatewayd-client.c
@@ -1,38 +1,80 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-
 #include "czmq.h"
 #include "zmq.h"
 
-int main(void)
-{
-    void *ctx = zctx_new ();
-    void *client = zsocket_new (ctx, ZMQ_DEALER);
-    zsocket_connect (client, "tcp://localhost:5555");
+#define HEARTBEAT_INTERVAL 1000 // msecs
+#define SERVER_HEARTBEAT_INTERVAL 1000 // msecs
+#define HEARTBEAT "\001"
+#define END_OF_RESPONSE "\002"
 
-    char *string;
+/* Do sth with the received message */
+void response_handler(zmsg_t *response){
     zframe_t *frame;
+    char *frame_data;
     int more;
-
-    char *query_string = "{\"entries\": 5 , \"time1\" : 123 , \"time2\" : 789}";
-    zframe_t *query = zframe_new(query_string, strlen(query_string)+1);
-    zframe_send (&query, client, 0);
-
-    zmsg_t *response = zmsg_new ();
-    response = zmsg_recv(client);
-    printf("=== RECEIVED ===\n");
 
     do{
         frame = zmsg_pop (response);
         more = zframe_more (frame);
-        string = (char *) zframe_data (frame);
-        printf("FRAME: %s\n", string);
+        frame_data = (char *) zframe_data (frame);
+        if( strcmp( frame_data, HEARTBEAT) ) printf("<< GOT HEARTBEAT FROM SERVER >>\n");
+        else if( strcmp( frame_data, END_OF_RESPONSE) ) printf("<< FINISHED RESPONSE >>\n");
+        else printf("FRAME DATA: %s\n", frame_data);
         zframe_destroy (&frame);
     }while(more);
+    printf("\n\n");
 
-    zmsg_destroy (&response);
+}
 
+main(void){
+
+    /* initial setup */
+    zctx_t *ctx = zctx_new ();
+    void *client = zsocket_new (ctx, ZMQ_DEALER);
+    zsocket_connect (client, "tcp://localhost:5555");
+
+    /* send query */
+    char *query_string = "{\"entries\": 5 , \"time1\" : 123 , \"time2\" : 789}";
+    zstr_send (client, query_string, 0);
+    printf("<< QUERY SENT >>\n");
+
+    zmq_pollitem_t items [] = {
+        { client, 0, ZMQ_POLLIN, 0 },
+    };
+
+    zmsg_t *response;
+    int rc;
+
+    /* receive response while sending heartbeats */
+    uint64_t heartbeat_at = zclock_time () + HEARTBEAT_INTERVAL;
+    while (true) {
+
+        rc = zmq_poll (items, 1, SERVER_HEARTBEAT_INTERVAL * ZMQ_POLL_MSEC);
+        if (rc == 0){
+            printf("<< NO RESPONSE FROM SERVER ...  >>\n");
+            break;
+        }
+
+        /* receive message and do sth with it */
+        if (items[0].revents & ZMQ_POLLIN){ 
+            response = zmsg_recv(client);
+            response_handler(response);
+            zmsg_destroy (&response);
+        }
+
+        if (zclock_time () >= heartbeat_at) {
+            zstr_send (client, HEARTBEAT, 0);
+            heartbeat_at = zclock_time () + HEARTBEAT_INTERVAL;
+            printf("<< HEARTBEAT SENT >>\n");
+        }
+
+    }
+
+    /* clear everything up */
+    zsocket_destroy (ctx, client);
+    zctx_destroy (&ctx);
+    printf("<< EXIT >>\n");
     return 0;
 }
